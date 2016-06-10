b95c75138f815d0b0d92a9282a130bac67f06f3f
suhanimody <suhanimody@gmail.com>
2016-06-10 13:04:04 +0530

    1. Added UI to configure paths to be deleted.

--- KillSwitch/source/Form1.Designer.cs
+++ KillSwitch/source/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(463, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(516, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Configured Items";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(463, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(516, 77);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 239);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- KillSwitch/source/Form1.cs
+++ KillSwitch/source/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace KillSwitch
 {
     public partial class Form1 : Form
     {
+        List<string> lstFiles = new List<string>();
+        string filePath = @"C:\test.xml";
         public Form1()
         {
             InitializeComponent();
+            lstFiles = GetConfiguration(filePath);
+            dataGridView1.DataSource = lstFiles.Select(x => new { FileName = x }).ToList();
+        }
+
+        private void SetConfiguration(List<string> lstConfig, string filePath)
+        {
+            XmlSerializer _serializer = new XmlSerializer(typeof(List<string>));
+            using (var _stream = File.OpenWrite(filePath))
+            {
+                _serializer.Serialize(_stream, lstConfig);
+            }
+        }
+
+        private List<string> GetConfiguration(string filePath)
+        {
+            List<string> _lstConfig;
+            XmlSerializer _serializer = new XmlSerializer(typeof(List<string>));
+            using (var _stream = File.OpenRead(filePath))
+            {
+                _lstConfig = (List<string>)_serializer.Deserialize(_stream);
+                return _lstConfig;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog _fileDialog = new OpenFileDialog();
+            _fileDialog.Multiselect = true;
+            if (_fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = _fileDialog.FileName;
+                if (!lstFiles.Contains(textBox1.Text))
+                {
+                    lstFiles.Add(textBox1.Text);
+                }
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.DataSource = lstFiles.Select(x => new { FileName = x }).ToList();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SetConfiguration(lstFiles, filePath);
         }
     }
 }
